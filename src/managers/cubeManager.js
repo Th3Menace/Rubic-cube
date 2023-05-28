e02c0616e91aab02424f98657585ef7e9d0deac1
Minas Minasyan <min4o_ersa@abv.bg>
2023-05-28 12:16:40 +0100

    Add Id Generator

--- src/managers/cubeManager.js
+++ src/managers/cubeManager.js
@@ -1,13 +1,15 @@
+const uniqid = require('uniqid')
 const cubes = [];
+
+exports.getAll = () => cubes.slice();
 
 exports.create = (cubeData) => {
     const newCube = {
-        id:cubes.length + 1,
+        id:uniqid(),
         ...cubeData,
       }
+      
   cubes.push(newCube );
 
-return newCube
-
-
+return newCube;
 };  
