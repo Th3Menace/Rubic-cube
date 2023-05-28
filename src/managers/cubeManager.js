e3278bbf27e674b122957d53c43f9fdba6c18847
Minas Minasyan <min4o_ersa@abv.bg>
2023-05-28 12:00:05 +0100

    Add cubeManager with create logic

---
+++ src/managers/cubeManager.js
@@ -0,0 +1,13 @@
+const cubes = [];
+
+exports.create = (cubeData) => {
+    const newCube = {
+        id:cubes.length + 1,
+        ...cubeData,
+      }
+  cubes.push(newCube );
+
+return newCube
+
+
+};  
